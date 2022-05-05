a81ef9034bcf6bc31cd90b71bce8fffe48cc6950
Yi Huang <yi@secondstate.io>
2022-05-06 05:44:44 +0800

    [CI] Test with node.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+# XXX: workaround for $GITHUB_PATH
+export PATH=$GITHUB_WORKSPACE/$INPUT_PATH:$PATH
+# set and check WASI_RUNTIME
+export WASI_RUNTIME=$1
+which $WASI_RUNTIME
+# get and run libsodium benchmarks
 git clone --branch stable https://github.com/jedisct1/libsodium
 cd libsodium
-export WASI_RUNTIME=$1
-./dist-build/wasm32-wasi.sh --bench
+ ./dist-build/wasm32-wasi.sh --bench
